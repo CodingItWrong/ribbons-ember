c329764658226f5eadbb89f9772bec5b363e87f0
Josh Justice <josh.justice@accesscfa.com>
2020-06-26 17:39:38 -0400

    Fix auth on logged out page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,19 @@
 import Route from '@ember/routing/route';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
+  @service session;
+
   model() {
-    return this.store.query('reading', {
-      filter: { complete: false },
-      include: 'book',
-    });
+    if (this.session.get('isAuthenticated')) {
+      return this.store.query('reading', {
+        filter: { complete: false },
+        include: 'book',
+      });
+    } else {
+      return null;
+    }
   }
 
   @action
