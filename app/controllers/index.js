5bc9bcc13c4c32a4ae7453fbe630eb5a33f7536e
Josh Justice <josh.justice@accesscfa.com>
2020-06-28 10:26:58 -0400

    Fix 'another reading' button

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,10 +1,16 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class IndexController extends Controller {
   @service router;
 
   get hasMultipleReadings() {
     return this.model.length > 1;
+  }
+
+  @action
+  goToNewReadings() {
+    this.router.transitionTo('readings.new');
   }
 }
