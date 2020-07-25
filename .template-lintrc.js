50973a1aab5ac5bb91b6b8d2e3821b83a4a7e70e
Josh Justice <me@codingitwrong.com>
2020-07-25 09:32:04 -0400

    Fix other lint rule

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -4,5 +4,8 @@ module.exports = {
   extends: 'octane',
   rules: {
     'no-action': false, // this is the documented approach in ember-paper
+    'no-implicit-this': {
+      allow: ['send'],
+    },
   },
 };
