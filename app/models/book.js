18c69f6c70273a21da7616e4853fb0bbb7bd70ef
Josh Justice <me@codingitwrong.com>
2020-12-25 20:30:25 -0500

    Add computed decorators

--- app/models/book.js
+++ app/models/book.js
@@ -1,5 +1,6 @@
 import Model, { attr } from '@ember-data/model';
 import classic from 'ember-classic-decorator';
+import { computed } from '@ember/object';
 
 @classic
 export default class BookModel extends Model {
@@ -8,10 +9,12 @@ export default class BookModel extends Model {
   @attr numChapters;
   @attr('date') lastReadAt;
 
+  @computed('id')
   get numericId() {
     return Number(this.id);
   }
 
+  @computed('singularName', 'name')
   get nameForChapter() {
     return this.singularName || this.name;
   }
--- app/models/reading.js
+++ app/models/reading.js
@@ -1,5 +1,6 @@
 import Model, { attr, belongsTo } from '@ember-data/model';
 import classic from 'ember-classic-decorator';
+import { computed } from '@ember/object';
 
 @classic
 export default class ReadingModel extends Model {
@@ -9,10 +10,12 @@ export default class ReadingModel extends Model {
   @attr furthestReadChapter;
   @attr complete;
 
+  @computed('book.numChapters', 'furthestReadChapter')
   get allChaptersRead() {
     return this.furthestReadChapter >= this.book.get('numChapters');
   }
 
+  @computed('furthestReadChapter')
   get chapterToRead() {
     return this.furthestReadChapter + 1;
   }
