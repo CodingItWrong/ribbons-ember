ed1bc041d0cc63aa9a8a3b403f3a7e57b9b20c64
Josh Justice <me@codingitwrong.com>
2020-12-25 20:29:17 -0500

    Add classic decorator to all models

--- app/models/chapter-completion.js
+++ app/models/chapter-completion.js
@@ -1,5 +1,7 @@
 import Model, { attr, belongsTo } from '@ember-data/model';
+import classic from 'ember-classic-decorator';
 
+@classic
 export default class ChapterCompletionModel extends Model {
   @belongsTo('reading') reading;
   @attr chapter;
