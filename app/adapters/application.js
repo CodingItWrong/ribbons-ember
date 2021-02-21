1f52219021992cf32fb53129cf9f35aaf1d27164
Josh Justice <me@codingitwrong.com>
2021-02-21 14:40:43 -0500

    Remove unnecessary decorator for Paper

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,6 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 // eslint-disable-next-line ember/no-mixins
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
-// eslint-disable-next-line ember/no-computed-properties-in-native-classes
-import { computed } from '@ember/object';
 import ENV from '../config/environment';
 
 export default class ApplicationAdapter extends JSONAPIAdapter.extend(
@@ -10,11 +8,6 @@ export default class ApplicationAdapter extends JSONAPIAdapter.extend(
 ) {
   host = ENV.apiHost;
 
-  // TODO: remove this @computed decorator once we are migrated off Ember-Paper.
-  // Ember-Paper causes an "attempted to update" error upon login, preventing
-  // us from testing that this computed property really works without the
-  // decorator. It did not work in the past.
-  @computed('session.{isAuthenticated,data.authenticated.access_token}')
   get headers() {
     let headers = {};
 
