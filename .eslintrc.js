ba85d353f609a1bf67e1e2286e92b440471d613d
Josh Justice <me@codingitwrong.com>
2021-05-09 11:41:50 -0400

    Prevent duplicate imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
   },
   rules: {
     'import/order': ['warn', { alphabetize: { order: 'asc' } }], // group and then alphabetize lines - https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
+    'no-duplicate-imports': 'error',
     'sort-imports': [
       'warn',
       { ignoreDeclarationSort: true, ignoreMemberSort: false },
